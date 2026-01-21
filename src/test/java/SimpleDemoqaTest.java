c68112a651fc1fefe3b9f0ba2c23c3b35993cf10
Sasha Bezrodnov <garri348@gmail.com>
2026-01-21 19:45:22 +0500

    update test, build

--- src/test/java/SimpleDemoqaTest.java
+++ src/test/java/SimpleDemoqaTest.java
@@ -1,13 +1,43 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 public class SimpleDemoqaTest {
 
-    @Test
-    void firstTest() {
-        Assertions.assertTrue(3 > 2);
+    int result;
+
+   @BeforeAll
+   static void DlyaVsego(){
+       System.out.println("\n### Zapomni\n");
+   }
+
+    @BeforeEach
+    void Agaponyal(){
+        System.out.println("### Nastroika");
+        result = 3;
     }
 
+    @AfterEach
+    void Posle() {
+        System.out.println("### WorkCLOSE\n");
+        result = 0;
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("###      firstTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("###      secondTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void thirdTest() {
+        System.out.println("###      thirdTest()");
+        Assertions.assertTrue(result > 2);
+    }
 }
